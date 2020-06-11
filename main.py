84e7e64da47974ee740486fec2ac20528182628b
tmkadamcz <tmkadamcz@gmail.com>
2020-06-10 17:19:37 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,19 @@
+dist1 = {
+	family_and_parameters = {
+		family = ''
+		parameters = {}
+	}
+	weight = 
+	flip_sign = 
+	truncate_left_of_mode = 
+	x_translation = 
+}
+
+class dist():
+	def __init__(self,
+	family_and_parameters,
+	weight=0.5,
+	flip_sign=0,
+	truncate_left_of_mode=0,
+	x_translation=0,
+	):
